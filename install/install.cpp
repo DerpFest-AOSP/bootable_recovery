0a6f206b205fa6b99ccc610540e3450e1d196086
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-08-04 00:27:15 -0300

    Fully remove downgrade/verify checks for A/B
    Change-Id: Ib1dfbea7ca9c00ae8719d52f8952176bc9d56d51
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- install/install.cpp
+++ install/install.cpp
@@ -174,7 +174,7 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
     return false;
   }
 
-  // Check for downgrade version.
+/*  // Check for downgrade version.
   bool undeclared_downgrade = false;
   int64_t build_timestamp =
       android::base::GetIntProperty("ro.build.date.utc", std::numeric_limits<int64_t>::max());
@@ -200,8 +200,7 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
   if (undeclared_downgrade &&
       !(ui->IsTextVisible() && ask_to_continue_downgrade(ui->GetDevice()))) {
     return false;
-  }
-
+  }*/
   return true;
 }
 
