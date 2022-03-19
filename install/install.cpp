82c5bfbadc9570f3eb116aa128b39be01484c090
vjspranav <pranavasri@live.in>
2022-03-19 23:02:54 +0530

    Skip verify on install

--- install/install.cpp
+++ install/install.cpp
@@ -174,9 +174,9 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
     return false;
   }
 
-/*  // Check for downgrade version.
+  // Check for downgrade version.
   bool undeclared_downgrade = false;
-  int64_t build_timestamp =
+  /*int64_t build_timestamp =
       android::base::GetIntProperty("ro.build.date.utc", std::numeric_limits<int64_t>::max());
   int64_t pkg_post_timestamp = 0;
   // We allow to full update to the same version we are running, in case there
@@ -196,11 +196,11 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
       undeclared_downgrade = true;
     }
   }
-
+  */
   if (undeclared_downgrade &&
       !(ui->IsTextVisible() && ask_to_continue_downgrade(ui->GetDevice()))) {
     return false;
-  }*/
+  }
   return true;
 }
 
